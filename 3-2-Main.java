f7aa3f89672e18b39ec6c82ae31e48e55c489e2b
李曌珩 <zhaohengli.thu@gmail.com>
2019-08-15 12:30:40 +0800

    Update 3-2-Main.java

--- 3-2-Main.java
+++ 3-2-Main.java
@@ -1,1 +1,27 @@
 
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String match = scan.nextLine().toLowerCase();
+		scan.close();
+		
+		String dirName = "input/test/case";
+		File dir = new File(dirName);
+		String[] files = dir.list();
+		
+		Vector<String> result = new Vector<String>();
+        for (int i = 0; i < files.length; i++) {
+        	File subFile = new File(dirName+"/"+files[i]);
+        	if (subFile.isFile()) {
+        		String fileName = files[i].toLowerCase();
+        		if (fileName.contains(match)) result.add(files[i]);
+        	}
+        }
+        
+        Collections.sort(result);
+        for (String i : result) System.out.println(i);
+	}
+}
